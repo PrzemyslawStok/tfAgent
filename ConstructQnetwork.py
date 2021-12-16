50faaec1fad2e4062afe6f697cfb1c3c26234309
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-12-16 22:15:24 +0100

    cd..

---
+++ ConstructQnetwork.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tf_agents.networks import sequential, q_network
+
+
+# Define a helper function to create Dense layers configured with the right
+# activation and kernel initializer.
+def dense_layer(num_units):
+    return tf.keras.layers.Dense(
+        num_units,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+
+# QNetwork consists of a sequence of Dense layers followed by a dense layer
+# with `num_actions` units to generate one q_value per available action as
+def construct_qnet(numActions: int, networkStruct: tuple = (100, 50)):
+    dense_layers = [dense_layer(num_units) for num_units in networkStruct]
+    q_values_layer = tf.keras.layers.Dense(
+        numActions,
+        activation=None,
+        kernel_initializer=tf.keras.initializers.RandomUniform(
+            minval=-0.03, maxval=0.03),
+        bias_initializer=tf.keras.initializers.Constant(-0.2))
+    q_net = sequential.Sequential(dense_layers + [q_values_layer])
+    return q_net
