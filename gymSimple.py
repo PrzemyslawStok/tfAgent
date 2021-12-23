c88b5d46dea5fa826902ff5d1e1b91bd35a7994f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-12-23 14:15:43 +0100

    cd..

--- gymSimple.py
+++ gymSimple.py
@@ -16,14 +16,15 @@ from tf_agents.specs import tensor_spec
 agentDir = "savedAgents"
 
 
-def renderEnv(envName="CartPole-v0", doneAfterEnd=True, closeEnv=False, steps=100, epizodes=1):
+def renderEnv(envName="CartPole-v0", policy=None, doneAfterEnd=True, closeEnv=False, steps=100, epizodes=1):
     env = gym.make(envName)
     env1 = env.unwrapped
 
     time_spec = ts.time_step_spec(suite_gym.gym_wrapper.spec_from_gym_space(env.observation_space))
 
-    random_policy = random_tf_policy.RandomTFPolicy(time_spec,
-                                                    suite_gym.gym_wrapper.spec_from_gym_space(env.action_space))
+    if policy == None:
+        policy = random_tf_policy.RandomTFPolicy(time_spec,
+                                                suite_gym.gym_wrapper.spec_from_gym_space(env.action_space))
 
     for episode in range(epizodes):
         observation = env.reset()
@@ -35,7 +36,7 @@ def renderEnv(envName="CartPole-v0", doneAfterEnd=True, closeEnv=False, steps=10
             time_step = ts.transition(tf.expand_dims(tf.convert_to_tensor(observation), axis=0),
                                       tf.expand_dims(tf.convert_to_tensor(reward), axis=0))
 
-            action = random_policy.action(time_step).action
+            action = policy.action(time_step).action
             observation, reward, done, info = env.step(action.numpy()[0])
 
             if doneAfterEnd and done:
@@ -66,9 +67,5 @@ def loadAgent(agentDir: str, savedPolicy: str):
 if __name__ == "__main__":
     agent = loadAgent(agentDir, "Cartpole-v1-trained-agent")
     # renderEnv("LunarLander-v2")
-    renderEnv()
-    printEnvNames()
-
-    space = spaces.Discrete(10)
-
-    print(space)
+    renderEnv(envName="CartPole-v0", policy=agent)
+    # printEnvNames()
