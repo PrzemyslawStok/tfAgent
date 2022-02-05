eada410a78bf37cc3dc0768eac501ad2df018e09
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-05 21:14:22 +0100

    cd..

--- CustomEnv/dqn.py
+++ CustomEnv/dqn.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
+from keras.utils.generic_utils import Progbar
 from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.environments import tf_py_environment
+from tf_agents.metrics import tf_metrics
 
 from tf_agents.networks import q_network
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.utils import common
 
-from GraphEnv import GraphEnv, sampleEnv
+from GraphEnv import sampleEnv
 
 replay_buffer_max_length = 100000
 batch_size = 64
 learning_rate = 1e-3
+
+num_iterations = 10_000
 
 
 def create_qnet(fc_layer_params: tuple, train_env: tf_py_environment) -> q_network:
@@ -26,7 +31,6 @@ if __name__ == "__main__":
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     train_step_counter = tf.Variable(0)
-
 
     fc_layer_params = (64, 256)
     q_net = create_qnet(fc_layer_params, train_env)
@@ -49,3 +53,34 @@ if __name__ == "__main__":
         data_spec=agent.collect_data_spec,
         batch_size=train_env.batch_size,
         max_length=replay_buffer_max_length)
+
+    replay_observer = [replay_buffer.add_batch]
+
+    dataset = replay_buffer.as_dataset(
+        num_parallel_calls=2,
+        sample_batch_size=batch_size,
+        num_steps=2).prefetch(2)
+
+    dataset_iterator = iter(dataset)
+
+    train_metrics = [
+        tf_metrics.NumberOfEpisodes(),
+        tf_metrics.EnvironmentSteps(),
+        tf_metrics.AverageReturnMetric(),
+        tf_metrics.AverageEpisodeLengthMetric(),
+    ]
+
+    driver = dynamic_episode_driver.DynamicEpisodeDriver(train_env,
+                                                         agent.collect_policy,
+                                                         observers=replay_observer + train_metrics,
+                                                         num_episodes=1)
+
+    metrics_names = ['reward', 'length']
+    progbar = Progbar(num_iterations, stateful_metrics=metrics_names)
+
+    final_time_step, policy_state = driver.run()
+
+    for i in range(num_iterations):
+        final_time_step, _ = driver.run(final_time_step)
+
+        experience, _ = next(dataset_iterator)
