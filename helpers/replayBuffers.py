cf8eeb038d7644c7ffb78bc0d60e3391507dd7fd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-08 00:12:16 +0100

    cd..

--- helpers/replayBuffers.py
+++ helpers/replayBuffers.py
@@ -1,7 +1,13 @@
+import random
+from collections import deque
+
 import tensorflow as tf
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 
+import numpy as np
+
 if __name__ == "__main__":
+
     data_spec = tf.TensorSpec([3], tf.float32, "action")
     batch_size = 2
     max_length = 1000
@@ -16,10 +22,27 @@ if __name__ == "__main__":
 
     value = tf.ones([3])
     batched1 = tf.expand_dims(value, axis=0)
-    batched = tf.nest.map_structure(lambda t: tf.stack([t] * batch_size), 1)
+    batched = tf.nest.map_structure(lambda t: tf.stack([t] * batch_size), value)
 
     batched2 = tf.stack([value])
 
     # Version with ragged tensor
 
-    replay_buffer.add_batch(batched)
+    #replay_buffer.add_batch(batched)
+
+    for i in range(5):
+        value = tf.ones([3]) * i
+        #batch = tf.nest.map_structure(lambda x: tf.stack[x] * batch_size, value)
+        replay_buffer.add_batch(tf.stack([value]*batch_size))
+
+
+
+    replay_deque = deque(maxlen=100)
+
+    for i in range(100):
+        replay_deque.append(value)
+    replay_buffer.add_batch(tf.stack(random.sample(replay_deque, batch_size)))
+
+
+
+    print(2 * [1, 2])
