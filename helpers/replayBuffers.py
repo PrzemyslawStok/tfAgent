9b7fd7535ca021487a1e16dab7f040ead75d7186
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-08 23:43:02 +0100

    `cd..`

--- helpers/replayBuffers.py
+++ helpers/replayBuffers.py
@@ -6,8 +6,8 @@ from tf_agents.replay_buffers import tf_uniform_replay_buffer
 
 import numpy as np
 
-if __name__ == "__main__":
 
+def replay_buffer_1():
     data_spec = tf.TensorSpec([3], tf.float32, "action")
     batch_size = 2
     max_length = 1000
@@ -29,12 +29,12 @@ if __name__ == "__main__":
 
     # Version with ragged tensor
 
-    #replay_buffer.add_batch(batched)
+    # replay_buffer.add_batch(batched)
 
     for i in range(5):
         value = tf.ones([3]) * i
-        #batch = tf.nest.map_structure(lambda x: tf.stack[x] * batch_size, value)
-        replay_buffer.add_batch(tf.stack([value]*batch_size))
+        # batch = tf.nest.map_structure(lambda x: tf.stack[x] * batch_size, value)
+        replay_buffer.add_batch(tf.stack([value] * batch_size))
 
     replay_deque = deque(maxlen=100)
 
@@ -43,3 +43,20 @@ if __name__ == "__main__":
 
     replay_buffer.add_batch(tf.stack(random.sample(replay_deque, batch_size)))
 
+
+def replay_buffer_simple():
+    size = 2
+    data_spec_simple = tf.TensorSpec([1], tf.int32, "simple_data")
+    replay_buffer_simple = tf_uniform_replay_buffer.TFUniformReplayBuffer(data_spec_simple, batch_size=size,
+                                                                          max_length=100)
+
+    for i in range(10):
+        value_simple = i * tf.ones([1], tf.int32)
+        batch_simple = tf.stack([value_simple] * size)
+        replay_buffer_simple.add_batch(batch_simple)
+
+    print(replay_buffer_simple)
+
+
+if __name__ == "__main__":
+    replay_buffer_simple()
