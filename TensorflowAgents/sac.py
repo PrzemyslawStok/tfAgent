47bb852a2f5bc36b907c159883e5db0085810b27
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-14 20:15:27 +0100

    `cd..`

--- TensorflowAgents/sac.py
+++ TensorflowAgents/sac.py
@@ -8,63 +8,60 @@ import tensorflow as tf
 import tf_agents
 from matplotlib import pyplot as plot
 from tensorflow.keras.utils import Progbar
+from tf_agents.agents.ddpg import critic_network
 from tf_agents.agents.dqn import dqn_agent
+from tf_agents.agents.sac import tanh_normal_projection_network
 from tf_agents.drivers import dynamic_step_driver, dynamic_episode_driver, py_driver
 from tf_agents.environments import ParallelPyEnvironment, suite_pybullet
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 from tf_agents.metrics import tf_metrics
-from tf_agents.networks import q_network
+from tf_agents.networks import q_network, actor_distribution_network
 from tf_agents.policies import random_tf_policy, policy_saver, py_tf_eager_policy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.specs import tensor_spec
-from tf_agents.train.utils import strategy_utils, spec_utils
+from tf_agents.train.utils import strategy_utils, spec_utils, train_utils
 from tf_agents.utils import common
 
 import timeit
 
 tempdir = tempfile.gettempdir()
 
-env_name = "MinitaurBulletEnv-v0" # @param {type:"string"}
+env_name = "MinitaurBulletEnv-v0"  # @param {type:"string"}
 
 # Use "num_iterations = 1e6" for better results (2 hrs)
 # 1e5 is just so this doesn't take too long (1 hr)
-num_iterations = 100000 # @param {type:"integer"}
+num_iterations = 100000  # @param {type:"integer"}
 
-initial_collect_steps = 10000 # @param {type:"integer"}
-collect_steps_per_iteration = 1 # @param {type:"integer"}
-replay_buffer_capacity = 10000 # @param {type:"integer"}
+initial_collect_steps = 10000  # @param {type:"integer"}
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_capacity = 10000  # @param {type:"integer"}
 
-batch_size = 256 # @param {type:"integer"}
+batch_size = 256  # @param {type:"integer"}
 
-critic_learning_rate = 3e-4 # @param {type:"number"}
-actor_learning_rate = 3e-4 # @param {type:"number"}
-alpha_learning_rate = 3e-4 # @param {type:"number"}
-target_update_tau = 0.005 # @param {type:"number"}
-target_update_period = 1 # @param {type:"number"}
-gamma = 0.99 # @param {type:"number"}
-reward_scale_factor = 1.0 # @param {type:"number"}
+critic_learning_rate = 3e-4  # @param {type:"number"}
+actor_learning_rate = 3e-4  # @param {type:"number"}
+alpha_learning_rate = 3e-4  # @param {type:"number"}
+target_update_tau = 0.005  # @param {type:"number"}
+target_update_period = 1  # @param {type:"number"}
+gamma = 0.99  # @param {type:"number"}
+reward_scale_factor = 1.0  # @param {type:"number"}
 
 actor_fc_layer_params = (256, 256)
 critic_joint_fc_layer_params = (256, 256)
 
-log_interval = 5000 # @param {type:"integer"}
+log_interval = 5000  # @param {type:"integer"}
 
-num_eval_episodes = 20 # @param {type:"integer"}
-eval_interval = 10000 # @param {type:"integer"}
+num_eval_episodes = 20  # @param {type:"integer"}
+eval_interval = 10000  # @param {type:"integer"}
 
-policy_save_interval = 5000 # @param {type:"integer"}
+policy_save_interval = 5000  # @param {type:"integer"}
 
 if __name__ == '__main__':
     env = suite_pybullet.load(env_name)
     env.reset()
-    image = Image.fromarray(env.render())
-    image.show()
-
-    print('Observation Spec:')
-    print(env.time_step_spec().observation)
-    print('Action Spec:')
-    print(env.action_spec())
+    # image = Image.fromarray(env.render())
+    # image.show()
 
     collect_env = suite_pybullet.load(env_name)
     eval_env = suite_pybullet.load(env_name)
@@ -75,6 +72,27 @@ if __name__ == '__main__':
     observation_spec, action_spec, time_step_spec = (
         spec_utils.get_tensor_specs(collect_env))
 
+    print('Observation Spec:')
+    print(observation_spec)
+    print('Action Spec:')
+    print(action_spec)
+    print('TimeStep Spec:')
+    print(time_step_spec)
 
+    with strategy.scope():
+        critic_net = critic_network.CriticNetwork(
+            (observation_spec, action_spec),
+            observation_fc_layer_params=None,
+            action_fc_layer_params=None,
+            joint_fc_layer_params=critic_joint_fc_layer_params,
+            kernel_initializer='glorot_uniform',
+            last_kernel_initializer='glorot_uniform')
 
+        actor_net = actor_distribution_network.ActorDistributionNetwork(
+            observation_spec,
+            action_spec,
+            fc_layer_params=actor_fc_layer_params,
+            continuous_projection_net=(
+                tanh_normal_projection_network.TanhNormalProjectionNetwork))
 
+        train_step = train_utils.create_train_step()
