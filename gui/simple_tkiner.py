c1e4a88259d434a1e9740df03bb111b966c62d26
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-22 17:06:02 +0100

    `cd..`

--- gui/simple_tkiner.py
+++ gui/simple_tkiner.py
@@ -5,41 +5,18 @@ from tkinter import filedialog
 import cv2
 
 
-def button_command(a: int, b: int):
-    return lambda: print(f"button[{a}][{b}]")
-
-
-def window1(window: tk.Tk):
-    xdim = 5
-    ydim = 5
-
-    for i in range(xdim):
-        for j in range(ydim):
-            window.columnconfigure(i, weight=1, minsize=75)
-            window.rowconfigure(i, weight=1, minsize=50)
-
-            colorRand = np.random.randint(10, 99, [3])
-            frame = tk.Frame(master=window, relief=tk.RAISED, background=f"#{colorRand[0]}{colorRand[1]}{colorRand[2]}")
-            frame.grid(row=i, column=j)
-            label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-            button = tk.Button(master=frame, text=f"Row {i}\nColumn {j}", command=button_command(i, j))
-            label.pack(padx=5, pady=5)
-            button.pack(padx=5, pady=5)
-
-
 def select_file() -> str:
     path = filedialog.askopenfilename()
     return path
-
 
 def load_image(path: str, label: tk.Label):
     image = cv2.imread(path)
     image = Image.fromarray(image)
     image = image.resize((500, 500))
-    img = ImageTk.PhotoImage(image)
+    image = ImageTk.PhotoImage(image)
 
-    label.config(image=img)
-    label.image = img
+    label.config(image=image)
+    label.image = image
 
 
 def open_image(label: tk.Label):
@@ -50,9 +27,7 @@ def open_image(label: tk.Label):
     load_image(path, label)
 
 
-img = None
-
-def window2(window: tk.Tk, buttonsNo: int = 5) -> None:
+def create_view(window: tk.Tk, buttonsNo: int = 5) -> None:
     window.minsize(700, 500)
 
     right_frame = tk.Frame(master=window, bg="gray", width=500, height=500)
@@ -68,8 +43,9 @@ def window2(window: tk.Tk, buttonsNo: int = 5) -> None:
         button = tk.Button(master=left_frame, text=f"Button_{i}", command=lambda: open_image(label))
         button.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
+
 if __name__ == "__main__":
     window = tk.Tk()
-    window2(window)
+    create_view(window)
 
     window.mainloop()
