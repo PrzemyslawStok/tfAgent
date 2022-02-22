1b25e52c3bb041a1bea36ec496d57b9ae77fcf78
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-02-22 17:03:54 +0100

    `cd..`

--- gui/simple_tkiner.py
+++ gui/simple_tkiner.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import numpy as np
+from PIL import Image, ImageTk
+from tkinter import filedialog
+import cv2
 
 
 def button_command(a: int, b: int):
@@ -24,19 +27,46 @@ def window1(window: tk.Tk):
             button.pack(padx=5, pady=5)
 
 
+def select_file() -> str:
+    path = filedialog.askopenfilename()
+    return path
+
+
+def load_image(path: str, label: tk.Label):
+    image = cv2.imread(path)
+    image = Image.fromarray(image)
+    image = image.resize((500, 500))
+    img = ImageTk.PhotoImage(image)
+
+    label.config(image=img)
+    label.image = img
+
+
+def open_image(label: tk.Label):
+    path = select_file()
+    if (len(path) == 0):
+        return
+
+    load_image(path, label)
+
+
+img = None
+
 def window2(window: tk.Tk, buttonsNo: int = 5) -> None:
     window.minsize(700, 500)
 
     right_frame = tk.Frame(master=window, bg="gray", width=500, height=500)
     left_frame = tk.Frame(master=window, bg="darkgray", width=200, height=500)
 
+    label = tk.Label(master=right_frame, text="Load image")
+    label.pack(fill=tk.BOTH, expand=True)
+
     right_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
     left_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
     for i in range(buttonsNo):
-        button = tk.Button(master=left_frame, text=f"Button_{i}")
+        button = tk.Button(master=left_frame, text=f"Button_{i}", command=lambda: open_image(label))
         button.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
-
 
 if __name__ == "__main__":
     window = tk.Tk()
